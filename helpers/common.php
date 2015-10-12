94c152789cc9a17e3a6e9a67ab859860b347d24e
Derek Williams <betteryourweb@gmail.com>
2015-10-12 16:13:45 -0500

    Adding a global helper file (helpers/common.php)

--- helpers/common.php
+++ helpers/common.php
@@ -5,3 +5,13 @@ use Betteryourweb\Helpers\Common;
 function base_dir($path){
 	return Common::base_dir($path);
 }
+
+function print_var_name($var) {
+    foreach($GLOBALS as $var_name => $value) {
+        if ($value === $var) {
+            return $var_name;
+        }
+    }
+
+    return false;
+}
