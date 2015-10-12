310a538086c79bff2dcc13da6e19f73d3cc6502e
Derek Williams <betteryourweb@gmail.com>
2015-10-12 15:32:32 -0500

    Added base_dir method in the Betteryourweb\Helpers\Common.php file

--- src/Common.php
+++ src/Common.php
@@ -1,7 +1,17 @@
 <?php
 namespace Betteryourweb\Helpers;
 
+use Betteryourweb\Helpers\Exception;
 class Common
 {
-	
+	public function __construct(){
+		if(!defined('BASE_DIR')){
+			throw new Exception("The base directory is not defined. Please define BASE_DIR to use this function");
+
+		}
+	}
+	public static function base_dir($path){
+		if(!$path) return BASE_DIR;
+		return BASE_DIR."/".$path;
+	}
 }
